97f6ff1321759a9080ed04be8dc73b50148300a1
zhanping <raozhanping@gmail.com>
2020-08-20 14:42:43 +0800

    chore: use terser to minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,23 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
+import { terser } from 'rollup-plugin-terser'
 import path from 'path'
 const packageJson = require('./package.json')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const plugins = [
+  peerDepsExternal(),
+  resolve(),
+  commonjs(),
+  typescript({ useTsconfigDeclarationDir: true }),
+  postcss(),
+]
+
+if (isProd) {
+  plugins.push(terser())
+}
 
 export default {
   context: path.resolve(__dirname, 'src'),
@@ -22,11 +37,5 @@ export default {
     },
   ],
   preserveModules: false,
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    postcss(),
-  ],
+  plugins: plugins,
 }
